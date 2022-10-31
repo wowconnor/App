f1da935baae88f1eab9d1896df9f7c5c9af518cb
Connor Marks <connormarks14@gmail.com>
2022-10-31 14:11:06 -0700

    Added Text

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Christian smells.");
+        System.out.println("Jackie smells.");
+        System.out.println("Darren smells.");
+        System.out.println("Connor rules.");
+    }
+}
