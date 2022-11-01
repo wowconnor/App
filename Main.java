8b1b0cfef8c0f4b84a4a3fe54e081d9bf6700c01
Connor Marks <connormarks14@gmail.com>
2022-10-31 18:40:00 -0700

    Added Text

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args){
+        System.out.println("\n******************");
         System.out.println("Christian smells.");
         System.out.println("Jackie smells.");
         System.out.println("Darren smells.");
         System.out.println("Connor rules.");
+        System.out.println("******************");
 
 
         System.out.println("\nIf you changed this code, increment i!");
