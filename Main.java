4ba2fa62420496e15fee5c299e4aa78be6b0c33c
Jacqueline <jacqueline.gracey@gmail.com>
2022-10-31 19:09:16 -0700

    testing commit

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args){
         System.out.println("\n******************");
         System.out.println("Christian smells.");
-        System.out.println("Jackie smells.");
+        System.out.println("Jackie smells good.");
         System.out.println("Darren smells.");
-        System.out.println("Connor rules.");
+        System.out.println("Connor sucks pp.");
         System.out.println("******************");
 
 
         System.out.println("\nIf you changed this code, increment i!");
-        int i = 0;
+        int i = 1;
         System.out.println(i);
     }
 }
